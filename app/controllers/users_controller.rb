4e1ba213d8b6f14898111b5e0bba4953a89abecc
jmagero <jmagero2017@gmail.com>
2020-08-25 19:45:39 +0300

    Add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  include UsersHelper
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+end
