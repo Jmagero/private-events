a619a5cfbc57d2ccddc72239f03eac7b4e512ed6
Gui Recordon <guilherme.recordon@gmail.com>
2020-08-25 17:55:12 -0300

    Create event model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_160143) do
+ActiveRecord::Schema.define(version: 2020_08_25_204612) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "users_id"
+    t.index ["users_id"], name: "index_events_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
