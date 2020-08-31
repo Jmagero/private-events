b71e2b3544ab14d748ef7b6093cd9ca07fb7cd8d
Jocyline Magero <jmagero2017@gmail.com>
2020-09-01 00:38:15 +0300

    Add description to event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_181331) do
+ActiveRecord::Schema.define(version: 2020_08_31_210804) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_26_181331) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
+    t.text "description"
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
