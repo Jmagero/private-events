b664856cc4df31a36c8bd6e580b9b21d686a390f
Jocyline Magero <jmagero2017@gmail.com>
2020-09-01 22:37:04 +0300

    Add association between attendee & event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_210804) do
+ActiveRecord::Schema.define(version: 2020_09_01_172314) do
+
+  create_table "event_managers", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_managers_on_event_id"
+    t.index ["user_id"], name: "index_event_managers_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
