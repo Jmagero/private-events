2007dd2a853d1cf6fb6cdab7344d564a79373d77
Jocyline Magero <jmagero2017@gmail.com>
2020-09-02 18:13:10 +0300

    Add column reset to Fixcolumn migration

--- db/migrate/20200901205844_fix_column_name.rb
+++ db/migrate/20200901205844_fix_column_name.rb
@@ -3,6 +3,8 @@ class FixColumnName < ActiveRecord::Migration[5.2]
     change_table :event_managers do |t|
       t.rename :user_id, :attendee_id
       t.rename :event_id, :attended_event_id
+      EventManager.reset_column_information
+
     end
   end
 end
