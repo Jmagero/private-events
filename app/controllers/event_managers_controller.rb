e62a732271ed61732579cd5800071f6b7ef59a81
Jocyline Magero <jmagero2017@gmail.com>
2020-09-03 19:06:29 +0300

    Fix merge conflict

--- app/controllers/event_managers_controller.rb
+++ app/controllers/event_managers_controller.rb
@@ -1,9 +1,6 @@
 class EventManagersController < ApplicationController
-<<<<<<< HEAD
-=======
   
   def create
     @event_manager = Eventmanager.create(attendee_id = params[:user])
   end
->>>>>>> 0e81709d439beffb0ee96ce851de7d487e7e54cb
 end
