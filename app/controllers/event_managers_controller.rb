4843c4d1d249975ca98d428bd2fc6ddd640b2403
Gui Recordon <guilherme.recordon@gmail.com>
2020-09-03 16:30:21 -0300

    Attempt to activate invites

--- app/controllers/event_managers_controller.rb
+++ app/controllers/event_managers_controller.rb
@@ -1,6 +1,15 @@
 class EventManagersController < ApplicationController
-  
+  def new
+    @event_manager = EventManager.new
+    @users = User.all
+  end
+
   def create
-    @event_manager = Eventmanager.create(attendee_id = params[:user])
+    @event_manager = EventManager.new(event_manager_params)
+    @event_manager.save
+  end
+
+  def event_manager_params
+    params.require(:event_manager).permit(:attendee_id, :attended_event_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users 
   resources :events
   resources :sessions
+  resources :event_managers
 
   root 'events#index'
 end
