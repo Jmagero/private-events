a47d6d680e919f0270ba54e175aa31b547d26565
Gui Recordon <guilherme.recordon@gmail.com>
2020-09-03 18:06:27 -0300

    New attempt at invitations

--- app/controllers/event_managers_controller.rb
+++ app/controllers/event_managers_controller.rb
@@ -6,13 +6,13 @@ class EventManagersController < ApplicationController
 
   def create
     @event_manager = EventManager.new(event_manager_params)
+    
     if @event_manager.save
-        redirect_to root_path, notice: "You send the invitation!"
+        redirect_to root_path, notice: "You sent the invitation!"
     else
-        redirect_to root_path, notice: "An error happened you can't send the invitation."
+        redirect_to root_path, notice: "An error happened and you can't send the invitation."
     end
   end
-
 
   def event_manager_params
     params.require(:event_manager).permit(:attendee_id, :attended_event_id)
