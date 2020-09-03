6a8d5bf8f45e93673ca9736581ad20002740bcab
Jocyline Magero <jmagero2017@gmail.com>
2020-09-03 23:06:21 +0300

    Add create method to eventmanager

--- app/controllers/event_managers_controller.rb
+++ app/controllers/event_managers_controller.rb
@@ -6,10 +6,16 @@ class EventManagersController < ApplicationController
 
   def create
     @event_manager = EventManager.new(event_manager_params)
-    @event_manager.save
+    if @event_manager.save
+        redirect_to root_path, notice: "You send the invitation!"
+    else
+        redirect_to root_path, notice: "An error happened you can't send the invitation."
+    end
   end
+
 
   def event_manager_params
     params.require(:event_manager).permit(:attendee_id, :attended_event_id)
   end
+
 end
