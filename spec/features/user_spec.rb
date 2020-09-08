46458efe4d03b9e63127929dc85a6d9aa4ab40e1
Gui Recordon <guilherme.recordon@gmail.com>
2020-09-08 14:04:42 -0300

    Create tests for event associations

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe User, :type => :feature do
     end
   end
 
+  describe "the event creation process" do
+    it "creates a new event" do
+      visit 'users/new'
+      visit '/events/new' 
+      fill_in 'Description', with: 'event'
+      click_button 'Create Event'
+      expect(page).to have_content 'event'
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,24 @@ require "rails_helper"
 
 RSpec.describe User, :type => :model do
     context 'Association tests' do
-      it 'has many posts' do
+      it 'has many events' do
         user = User.reflect_on_association(:events)
         expect(user.macro).to eq(:has_many)
       end
     end
+
+    context 'Association tests' do
+      it 'has many event_managers' do
+        user = User.reflect_on_association(:event_managers)
+        expect(user.macro).to eq(:has_many)
+      end
+    end
+
+    context 'Association tests' do
+      it 'has attended_events' do
+        user = User.reflect_on_association(:attended_events)
+        expect(user.macro).to eq(:has_many)
+      end
+    end
+
 end
