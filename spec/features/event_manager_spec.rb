b8042812e0af6f7a57f6c84cd641a20d20755612
Gui Recordon <guilherme.recordon@gmail.com>
2020-09-09 17:33:24 -0300

    Change event manager spec

--- spec/features/event_manager_spec.rb
+++ spec/features/event_manager_spec.rb
@@ -2,21 +2,21 @@ require "rails_helper"
 
 RSpec.describe EventManager, :type => :feature do
     describe "the invitation process" do
-        let(:creator) { create(:user) }
+        let(:user) { User.create(username: "gui", email: "gui@example.com") }
         let(:invitee) { create(:user) }
-        let(:event) { build(:event) }
+        let(:event) { Event.create(name:"Great Event", date:"18-9-2020", description:"Description of a great event.") }
         it "invite a user to an event" do
             visit '/sessions/new' 
-            fill_in 'Username', with: 'User 0'
-            fill_in 'Email', with: '0@example.com'
+            fill_in 'username', with: user.username
+            fill_in 'email', with: user.email
             click_button 'Submit'
-            expect(page).to have_content 'user'
-            click_link 'Invite'
-            visit '/event_managers/new'
-          page.select(event.id, :from => "event_manager_attended_event_id")
-          page.select(user.id, :from => "user")
-          click_button 'Invite User'
-          visit root_path
+            expect(page).to have_content('Successfully logged in!')
+            click_link event.name
+          # page.select(event.id, :from => "event_manager_attended_event_id")
+          # page.select(user.id, :from => "user")
+          # click_button 'Invite User'
+          # visit root_path
+           # First we need to be in the root path
         end
     end
 end
