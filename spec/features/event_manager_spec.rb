e99277fc4ca2906335a64935861491039ca7a407
Gui Recordon <guilherme.recordon@gmail.com>
2020-09-09 13:08:24 -0300

    Add test sign up user

---
+++ spec/features/event_manager_spec.rb
@@ -0,0 +1,6 @@
+require "rails_helper"
+
+RSpec.describe EventManager, :type => :feature do
+  
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,13 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :selenium_chrome
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
