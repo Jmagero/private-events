710a5e419a93c14308ac8866f3a8978edb3a3416
Jocyline Magero <jmagero2017@gmail.com>
2020-09-10 19:02:08 +0300

    Attempt to test event_manager

--- spec/features/event_manager_spec.rb
+++ spec/features/event_manager_spec.rb
@@ -1,30 +1,29 @@
 require "rails_helper"
 
 RSpec.describe EventManager, :type => :feature do
-    describe "the invitation process" do
-        let(:user) { User.create(username: "gui", email: "gui@example.com") }
-        let(:event) { Event.create(name:"Great Event", date:"18-9-2020", description:"Description of a great event.") }
-        it "invite a user to an event" do
-            visit '/sessions/new' 
-            fill_in 'username', with: user.username
-            fill_in 'email', with: user.email
-            click_button 'Submit'
-            expect(page).to have_content('Successfully logged in!')
-            visit '/event_managers/new'
+    let(:user) { User.create(username: "gui", email: "gui@example.com") }
+    let(:event) { Event.create(name:"Great Event", date:"18-9-2020", description:"Description of a great event.") }
+    let(:creator) { User.create(username: "magz", email: "magz@example.com") }
+        describe "Invite user to an event" do
+            it "should invite user " do
+                visit new_event_manager_path
+                expect(page).to have_text('Invite a Friend')
+                page.select(event.id, :from => "event_manager_attended_event_id")
+                page.select(user.username, :from => 'selected_event')
+                click_link 'Invite User'
+                
+    
 
-            dropdown = all('select').first
+            # dropdown = all('select').first
 
-            select('Great Event', from: dropdown)
-            puts Event.all
-            dropdown = find('select#selected_event')
+            # select('Great Event', from: dropdown)
+            # puts Event.all
+            # dropdown = find('select#selected_event')
 
-            select('gui', from: dropdown)
+            # select('gui', from: dropdown)
 
-            click_button "Invite User"
-            expect(page).to have_content('You sent the invitation!')
-
-
-
+            # click_button "Invite User"
+            # expect(page).to have_content('You sent the invitation!')
           # page.select(event.id, :from => "event_manager_attended_event_id")
           # page.select(user.id, :from => "user")
           # click_button 'Invite User'
