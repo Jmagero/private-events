b4a2823a0656caada9b0a8153ef9b669f7f9cc75
Gui Recordon <guilherme.recordon@gmail.com>
2020-09-11 18:04:07 -0300

    Change destination after signup to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user)
+      redirect_to new_session_path
     else
       flash.alert = 'fields cannot be blank!'
       render 'new'
