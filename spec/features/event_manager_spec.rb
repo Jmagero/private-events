dd5792780f7b2b771f84fc83869764bdaa943d6f
Gui Recordon <guilherme.recordon@gmail.com>
2020-09-14 12:05:04 -0300

    Attempt to fix EventManager test

--- spec/features/event_manager_spec.rb
+++ spec/features/event_manager_spec.rb
@@ -5,19 +5,31 @@ RSpec.describe EventManager, type: :feature do
   let(:event) { Event.create(name: 'Great Event', date: '18-9-2020', description: 'Description of a great event.') }
   describe 'Invite user to an event' do
     it 'should invite user' do
+      visit '/users/new'
+      fill_in 'Username', with: 'gui'
+      fill_in 'Email', with: 'gui@example.com'
+      click_button 'Create User'
+      expect(page).to have_content 'Log_in'
       visit '/sessions/new'
-      fill_in 'Username', with: 'User 0'
-      fill_in 'Email', with: '0@example.com'
+      fill_in 'Username', with: 'gui'
+      fill_in 'Email', with: 'gui@example.com'
       click_button 'Submit'
-      expect(page).to have_content 'user'
-      click_button event.name
-      visit '/event/1'
-      click_button 'Invite'
+      expect(page).to have_content 'Successfully logged in!'
+      click_link 'Create Event'
+      visit '/events/new'
+      fill_in 'Name', with: 'Great Event'
+      fill_in 'Description', with: 'Description of a great event.'
+      fill_in 'Date', with: 9-12-2020
+      click_button 'Create Event'
+      expect(page).to have_content 'Great Event'
+      click_link 'Invite'
       visit new_event_manager_path
       expect(page).to have_text('Invite a Friend')
-      find('#event_manager_attended_event_id').find(:xpath, 'option[1]').select_option
+      find('#event_manager_attended_event_id').find(:xpath, 'option[1]').select_option         
       find('#event_manager_attendee_id').find(:xpath, 'option[1]').select_option
       click_button 'Invite User'
+      get root_path
+      assert_response :flash
       expect(page).to have_text('You sent the invitation!')
     end
   end
