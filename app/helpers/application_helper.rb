1311ce91fea5a11871c3ff073f82fe6c459e6c4e
Gui Recordon <guilherme.recordon@gmail.com>
2020-09-15 14:41:57 -0300

    Remove loggin from views to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_log_in
+    if current_user
+      link_to current_user.username, user_path(current_user)
+      link_to 'Log Out', session_path(current_user), method: :delete
+    else
+      link_to 'Sign up', new_user_path or
+      link_to 'Log In', new_session_path
+    end
+  end
+
 end
